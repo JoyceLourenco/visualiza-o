e55a508696132dad9ba1d7aec391598512c6e630
Rob Brennan <rob@therobbrennan.com>
2019-12-06 15:04:01 -0800

    Incorporate completed modules to round out dashboard

--- examples/14-using-d3-with-angular/src/app/app.module.ts
+++ examples/14-using-d3-with-angular/src/app/app.module.ts
@@ -1,16 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser'
-import { NgModule } from '@angular/core'
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component'
-import { TimelineComponent } from './timeline/timeline.component'
-import { ScatterComponent } from './scatter/scatter.component'
-import { HistogramComponent } from './histogram/histogram.component'
-import { ChartComponent } from './chart/chart.component'
-import { AxisComponent } from './chart/axis/axis.component'
-import { LineComponent } from './chart/line/line.component'
-import { CirclesComponent } from './chart/circles/circles.component'
-import { BarsComponent } from './chart/bars/bars.component'
-import { GradientComponent } from './chart/gradient/gradient.component'
+import { AppComponent } from "./app.component";
+// import { TimelineComponent } from './timeline/timeline.component'
+// import { ScatterComponent } from './scatter/scatter.component'
+// import { HistogramComponent } from './histogram/histogram.component'
+// import { ChartComponent } from './chart/chart.component'
+// import { AxisComponent } from './chart/axis/axis.component'
+// import { LineComponent } from './chart/line/line.component'
+// import { CirclesComponent } from './chart/circles/circles.component'
+// import { BarsComponent } from './chart/bars/bars.component'
+// import { GradientComponent } from './chart/gradient/gradient.component'
+import { TimelineComponent } from "./completed/timeline/timeline.component";
+import { ScatterComponent } from "./completed/scatter/scatter.component";
+import { HistogramComponent } from "./completed/histogram/histogram.component";
+import { ChartComponent } from "./completed/chart/chart.component";
+import { AxisComponent } from "./completed/chart/axis/axis.component";
+import { LineComponent } from "./completed/chart/line/line.component";
+import { CirclesComponent } from "./completed/chart/circles/circles.component";
+import { BarsComponent } from "./completed/chart/bars/bars.component";
+import { GradientComponent } from "./completed/chart/gradient/gradient.component";
 
 @NgModule({
   declarations: [
@@ -25,10 +34,8 @@ import { GradientComponent } from './chart/gradient/gradient.component'
     BarsComponent,
     GradientComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
