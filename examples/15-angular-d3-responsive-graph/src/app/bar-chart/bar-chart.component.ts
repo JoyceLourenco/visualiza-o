fd9a7561525487ba6a2095189342a1fd730f6219
Rob Brennan <rob@therobbrennan.com>
2019-12-09 11:08:04 -0800

    Added Input binding for bar chart data

--- examples/15-angular-d3-responsive-graph/src/app/bar-chart/bar-chart.component.ts
+++ examples/15-angular-d3-responsive-graph/src/app/bar-chart/bar-chart.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { DataModel } from './../data/data.model';
 
 @Component({
   selector: 'app-bar-chart',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bar-chart.component.css']
 })
 export class BarChartComponent implements OnInit {
+
+  @Input() data: DataModel[];
 
   constructor() { }
 
