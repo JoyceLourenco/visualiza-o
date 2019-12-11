d73d525b6b51b2e00fb71621fc2c1acb98110b9c
Rob Brennan <rob@therobbrennan.com>
2019-12-11 13:18:39 -0800

    Added getUpdates to market status service

--- examples/16-angular-d3-real-time-socket-io/app/src/app/market-status.service.ts
+++ examples/16-angular-d3-real-time-socket-io/app/src/app/market-status.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Subject, from } from 'rxjs';
+import * as socketio from 'socket.io-client';
 
 import { MarketPrice } from './market-price';
 
@@ -13,5 +15,22 @@ export class MarketStatusService {
 
   getInitialMarketStatus() {
     return this.httpClient.get<MarketPrice[]>(`${this.baseUrl}/api/market`);
+  }
+
+  getUpdates() {
+    // Create a manager for the Socket.IO endpoint at our specified URL
+    const socket = socketio(this.baseUrl);
+    // Create a RxJS Subject
+    const marketSubscription = new Subject<MarketPrice>();
+    // Create an Observable so consumers can listen to the updates
+    const marketSubscriptionObservable = from(marketSubscription);
+
+    // Socket.IO events
+    socket.on('market', (marketStatus: MarketPrice) => {
+      // Invoked whenever the Socket.IO endpoint publishes something new
+      marketSubscription.next(marketStatus);
+    });
+
+    return marketSubscriptionObservable;
   }
 }
