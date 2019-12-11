6de69c816cecf141bf7293687c1bea2979a3a00e
Rob Brennan <rob@therobbrennan.com>
2019-12-11 13:51:28 -0800

    Needed to add HttpClientTestingModule for testing the MarketStatusService

--- examples/16-angular-d3-real-time-socket-io/app/src/app/services/market-status.service.spec.ts
+++ examples/16-angular-d3-real-time-socket-io/app/src/app/services/market-status.service.spec.ts
@@ -1,9 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
 import { MarketStatusService } from './market-status.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('MarketStatusService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientTestingModule]
+  }));
 
   it('should be created', () => {
     const service: MarketStatusService = TestBed.get(MarketStatusService);
