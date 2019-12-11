c3783019bde093c39007f809f1cfb82767f21bd0
Rob Brennan <rob@therobbrennan.com>
2019-12-11 14:13:26 -0800

    Needed to add HttpClientTestingModule and the MarketChartComponent for testing the app component

--- examples/16-angular-d3-real-time-socket-io/app/src/app/components/app/app.component.spec.ts
+++ examples/16-angular-d3-real-time-socket-io/app/src/app/components/app/app.component.spec.ts
@@ -1,12 +1,13 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { MarketChartComponent } from '../market-chart/market-chart.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent, MarketChartComponent],
+      imports: [HttpClientTestingModule],
     }).compileComponents();
   }));
 
@@ -20,12 +21,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('app');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('app app is running!');
   });
 });
