7df2e7a79bdde41650ff263d9c62550ccaeffbbd
Rob Brennan <rob@therobbrennan.com>
2019-12-11 12:51:45 -0800

    Initial display of chart complete

--- examples/16-angular-d3-real-time-socket-io/app/src/app/app.component.ts
+++ examples/16-angular-d3-real-time-socket-io/app/src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { MarketStatusService } from './market-status.service';
+import { MarketPrice } from './market-price';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  marketStatus: MarketPrice[];
+  marketStatusToPlot: MarketPrice[];
+
+  set MarketStatus(status: MarketPrice[]) {
+    this.marketStatus = status;
+    this.marketStatusToPlot = this.marketStatus.slice(0, 20);
+  }
+
+  constructor(private marketStatusService: MarketStatusService) {
+    this.marketStatusService.getInitialMarketStatus().subscribe(prices => {
+      this.MarketStatus = prices;
+    });
+  }
 }
